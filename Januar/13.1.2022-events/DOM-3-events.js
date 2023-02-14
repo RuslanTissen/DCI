2d9b411095226522f752f3900417516aaa0138d9
SFERAS <rthyssen73@gmail.com>
2023-02-13 23:58:50 -0300

    some eventListeners added

--- Januar/13.1.2022-events/DOM-3-events.js
+++ Januar/13.1.2022-events/DOM-3-events.js
@@ -1,11 +1,37 @@
 const btn = document.querySelector("button")
 
-// function buttonClickHandler() {
-// 	console.log('Click');
-// }
-// btn.addEventListener("click", buttonClickHandler)
+function buttonClickHandler() {
+	console.log('Click');
+}
+btn.addEventListener("click", buttonClickHandler)
 // btn.removeEventListener("click", buttonClickHandler)
 
-btn.addEventListener("focus", ()=>{
-	console.log('Mouse enter!');
+btn.addEventListener("focus", () => { console.log('Focus!') })
+btn.addEventListener("blur", () => { console.log('BLUR') })
+
+const marquee = document.querySelector("#mark marquee")
+const original = marquee.innerHTML
+btn.addEventListener("click", () => {
+	if (marquee.innerHTML === original) {
+		marquee.innerHTML = "<q>Graaaaains!</q><br/>-Vegan zombie"
+	} else {
+		marquee.innerHTML = original
+	}
+	btn.classList.toggle("active")
+})
+
+/************ EVENT OBJECT ******************/
+
+const p = document.querySelector("p")
+p.addEventListener("click", (e) => {
+	console.log(e);
+	const element = document.createElement("div")
+	element.innerText = "Hello"
+	element.style.position = "absolute"
+	element.style.width = "50px"
+	element.style.height = "20px"
+	element.style.background = "white"
+	element.style.top = e.y + "px"
+	element.style.left = e.x +"px"
+	e.target.append(element)
 })
