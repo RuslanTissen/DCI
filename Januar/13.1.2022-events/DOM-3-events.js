9a2946cbce349554b9df762d64d62d2f63a56dcd
SFERAS <rthyssen73@gmail.com>
2023-02-14 22:52:40 -0300

    upodated after moths folders added

--- Januar/13.1.2022-events/DOM-3-events.js
+++ Januar/13.1.2022-events/DOM-3-events.js
@@ -1,37 +1,48 @@
-const btn = document.querySelector("button")
+document.addEventListener("DOMContentLoaded", () => {
+	console.log('Document is ready;)');
 
-function buttonClickHandler() {
-	console.log('Click');
-}
-btn.addEventListener("click", buttonClickHandler)
-// btn.removeEventListener("click", buttonClickHandler)
+	const btn = document.querySelector("button")
 
-btn.addEventListener("focus", () => { console.log('Focus!') })
-btn.addEventListener("blur", () => { console.log('BLUR') })
-
-const marquee = document.querySelector("#mark marquee")
-const original = marquee.innerHTML
-btn.addEventListener("click", () => {
-	if (marquee.innerHTML === original) {
-		marquee.innerHTML = "<q>Graaaaains!</q><br/>-Vegan zombie"
-	} else {
-		marquee.innerHTML = original
+	function buttonClickHandler() {
+		console.log('Click');
 	}
-	btn.classList.toggle("active")
-})
+	btn.addEventListener("click", buttonClickHandler)
+	// btn.removeEventListener("click", buttonClickHandler)
 
-/************ EVENT OBJECT ******************/
+	btn.addEventListener("focus", () => { console.log('Focus!') })
+	btn.addEventListener("blur", () => { console.log('BLUR') })
 
-const p = document.querySelector("p")
-p.addEventListener("click", (e) => {
-	console.log(e);
-	const element = document.createElement("div")
-	element.innerText = "Hello"
-	element.style.position = "absolute"
-	element.style.width = "50px"
-	element.style.height = "20px"
-	element.style.background = "white"
-	element.style.top = e.y + "px"
-	element.style.left = e.x +"px"
-	e.target.append(element)
+	const marquee = document.querySelector("#mark marquee")
+	const original = marquee.innerHTML
+	btn.addEventListener("click", () => {
+		if (marquee.innerHTML === original) {
+			marquee.innerHTML = "<q>Graaaaains!</q><br/>-Vegan zombie"
+		} else {
+			marquee.innerHTML = original
+		}
+		btn.classList.toggle("active")
+	})
+
+	/************ EVENT OBJECT ******************/
+
+	const p = document.querySelector("p")
+	p.addEventListener("click", (e) => {
+		console.log(e.target);
+
+		// const element = document.createElement("div")
+		// element.innerText = "Hello"
+		// element.style.position = "absolute"
+		// element.style.width = "50px"
+		// element.style.height = "20px"
+		// element.style.background = "white"
+		// element.style.top = e.y + "px"
+		// element.style.left = e.x +"px"
+		// e.target.append(element)
+		// document.body.append(element)
+	})
+
+	const input = document.querySelector("input")
+	input.addEventListener("keydown", (e) => {
+		console.log(e.key);
+	})
 })
