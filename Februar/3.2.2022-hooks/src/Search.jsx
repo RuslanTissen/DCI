5e94a6f004655f34f9041341c3466954404ec8bc
SFERAS <rthyssen73@gmail.com>
2023-02-18 00:15:57 -0300

    use effect continue

--- Februar/3.2.2022-hooks/src/Search.jsx
+++ Februar/3.2.2022-hooks/src/Search.jsx
@@ -3,19 +3,38 @@ import SearchResult from "./SearchResult.jsx"
 
 export default function Search() {
 	const [searchResults, setSearchResults] = useState([])
+	const [page, setPage] = useState(1)
+	const [loading, setLoading] = useState(true)
 
 	useEffect(() => {
-		const url = "https://api.github.com/search/repositories?q=react&per_page=100&page=1"
-
+		console.log('useEffect running');
+		const url = "https://api.github.com/search/repositories?q=react&per_page=100&page=" + page
+		setLoading(true)
 		fetch(url)
 			.then(response => response.json())
-			.then(result => setSearchResults(result.items))
-	}, [])
+			.then(result => {
+				setSearchResults(result.items)
+				setLoading(false)
+			})
+	}, [page])
+
+	function incrementPage() { setPage(page + 1) }
+	function decrementPage() { setPage(page - 1) }
 
 	return (
 		<div>
-			<h1>Search!</h1>
-			{searchResults.map(repo => <SearchResult repo={repo} test = "hello!" />)}
+			<h1>Search! - {page}</h1>
+
+			{loading === true && <Spiner />}
+			{loading === false && (
+				<div>
+					{page > 1 && <button onClick={decrementPage}>Prev</button>}
+					<button onClick={incrementPage}>Next</button>
+					{searchResults.map(item => <SearchResult key={item.id} repo={item} />)}
+				</div>
+			)}
+
+
 		</div>
 	)
 } 
--- Februar/3.2.2022-hooks/src/SearchResult.jsx
+++ Februar/3.2.2022-hooks/src/SearchResult.jsx
@@ -3,14 +3,11 @@
 export default function SearchResult(props) {
 
 	const repo = props.repo
-	const test = props.test
 
 	return (
 		<div>
 			{repo.full_name} -
 			<a href={repo.html_url}> Open repo</a>
-			<br />
-			{test}
 		</div>
 	)
 }
