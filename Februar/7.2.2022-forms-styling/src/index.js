40f6b87cb59bab25991d340ce3c062629edf95ff
SFERAS <rthyssen73@gmail.com>
2023-03-09 22:26:48 -0300

    VERSION 1 WUTH SEPARATE STATES

--- Februar/7.2.2022-forms-styling/src/index.js
+++ Februar/7.2.2022-forms-styling/src/index.js
@@ -35,5 +35,41 @@ function App() {
 	)
 }
 
+// VERSION WITH SEPARATE STATE VARIABLES:
+
+// function App() {
+// 	const [name, setName] = useState("Old Masker")
+// 	const [code, setCode] = useState("")
+
+// 	const handleNameChange = (e) => {
+// 		setName(e.target.value)
+// 	}
+// 	const handleCodeChange = (e) => {
+// 		setCode(e.target.value)
+// 	}
+
+// 	const handleSubmit = (e) => {
+// 		e.preventDefault()
+// 		// HERE Add your logic:
+// 		const data = {
+// 			name: name,
+// 			code: code
+// 		}
+// 		// Send the "data" to an API:
+// 	}
+
+// 	return (
+// 		<form onSubmit={handleSubmit}>
+// 			<div>
+// 				<input type="text" value={name} onChange={handleNameChange} />
+// 			</div>
+// 			<div>
+// 				<input type="text" value={code} onChange={handleCodeChange}></input>
+// 			</div>
+// 			<button>Add target to database</button>
+// 		</form>
+// 	)
+// }
+
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(<App />)
