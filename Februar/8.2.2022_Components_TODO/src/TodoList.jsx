4eadadb5561a26a8dc337417d8bb30e2c6afb24a
SFERAS <rthyssen73@gmail.com>
2023-05-07 23:20:45 -0300

    handle submit

--- Februar/8.2.2022_Components_TODO/src/TodoList.jsx
+++ Februar/8.2.2022_Components_TODO/src/TodoList.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function TodoList() {
-  return (
-	 <div>Todo List</div>
-  )
+	const [todos, setTodos] = useState([])
+	const [text, setText] = useState("")
+
+	const handleChange = (e) => {
+		setText(e.target.value)
+	}
+
+	const handleSubmit = (e) => {
+		e.preventDefaulte()
+		
+	}
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<input
+				type="text"
+				value={text}
+				onChange={handleChange}
+
+			/>
+			<ul>
+				<li>Buy coffee</li>
+				<li>Go for a walk</li>
+			</ul>
+		</form>
+	)
 }
 
 export default TodoList
