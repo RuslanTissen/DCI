df395e7925f47d63a3f52d959649b34acb996368
SFERAS <rthyssen73@gmail.com>
2023-05-18 21:34:01 -0300

    in progress

--- Februar/15.1.22_router-1/src/App.js
+++ Februar/15.1.22_router-1/src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { NavLink, Outlet, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function Login() {
@@ -10,7 +10,12 @@ function Contact() {
 }
 
 function Services() {
-	return <p>Services</p>
+	return (
+		<div>
+			<p>Services</p>
+			<Outlet />
+		</div>
+	)
 }
 function ServicesWeb() {
 	return <p>Services - Web</p>
@@ -19,16 +24,39 @@ function ServicesServer() {
 	return <p>Services - Server</p>
 }
 
+console.log("Aplication is starting")
 function App() {
 	return (
 		<div className="App">
-			<h1>Hello</h1>
+			<NavLink to="/"><h1>Hello</h1></NavLink>
+
+			{/* <nav>
+				<a href='/login'>Login</a> | {" "}
+				<a href='/contact'> Contact</a>
+			</nav> */}
+
+			<nav>
+				<NavLink to='login'>Login</NavLink> | {" "}
+				<NavLink to='contact'> Contact</NavLink> |
+				<NavLink to='services'> Services </NavLink> |
+				<NavLink to='services/web'> WEB </NavLink> |
+				<NavLink to='services/server'> SERVER </NavLink> |
+			</nav>
+
 			<Routes>
 				<Route path="login" element={<Login />} />
 				<Route path="contact" element={<Contact />} />
 
+				{/* 
 				<Route path="services" element={<Services />} >
 					<Route path="web" element={<ServicesWeb />} />
+					<Route path="server" element={<ServicesServer />} />
+				</Route > */}
+
+				<Route path="services" >
+					<Route index element={<Services />} />
+					<Route path="web" element={<ServicesWeb />} />
+					<Route path="server" element={<ServicesServer />} />
 				</Route >
 
 			</Routes>
