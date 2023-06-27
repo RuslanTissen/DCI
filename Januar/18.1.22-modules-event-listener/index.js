4bc760d9ff55cfaa87851dd694982dcb818a8ae1
SFERAS <rthyssen73@gmail.com>
2023-06-27 13:55:46 -0300

    fight with js file

--- Januar/18.1.22-modules-event-listener/index.js
+++ Januar/18.1.22-modules-event-listener/index.js
@@ -1,11 +1,15 @@
-const tree = document.querySelector("#tree")
+function myFunction(){
+		alert("hi!")
+	}
+
+const tree = document.getElementById("tree")
 
 tree.addEventListener("clilck" , (e) => {
-	if (e.target.TagName === "LI") {
+	if (e.target.TagName === "li") {
 		const list = e.target.querySelector("ul")
-		console.log(list)
+		console.log({list})
 		if (list.style.display === "none"){
-			list.style.display = "inital"
+			list.style.display = "block"
 		} else {
 			list.style.display = "none"
 		}
