052a3aa55553b4e271688496b299b636f1a0e5cf
SFERAS <rthyssen73@gmail.com>
2023-06-28 15:40:34 -0300

    add span and targeting parent element

--- Januar/18.1.22-modules-event-listener/index.js
+++ Januar/18.1.22-modules-event-listener/index.js
@@ -1,17 +1,33 @@
-function myFunction(){
-		alert("hi!")
-	}
+function myFunction() {
+	alert("hi!")
+}
 
 const tree = document.getElementById("tree")
-
-tree.addEventListener("click" , (e) => {
-	if (e.target.tagName === "LI") {
-		const list = e.target.querySelector("ul")
-		console.log({list})
-		if (list.style.display === "none"){
+tree.addEventListener("click", (e) => {
+	if (e.target.tagName === "UL") {
+		return;
+	}
+	let clickTarget = e.target;
+	if (e.target.tagName === "SPAN") {
+		clickTarget = e.target.parentElement;
+	}
+	if (clickTarget.tagName === "LI") {
+		const list = clickTarget.querySelector("ul")
+		if (!list) {
+			return;
+		}
+		if (list.style.display === "none") {
 			list.style.display = "block"
 		} else {
 			list.style.display = "none"
 		}
-	} 
+	}
+})
+
+const lis = document.querySelectorAll("li")
+lis.forEach(li => {
+	const span = document.createElement("span")
+	// span.prepend()
+	span.append(li.childNodes[0])
+	li.prepend(span)
 })
