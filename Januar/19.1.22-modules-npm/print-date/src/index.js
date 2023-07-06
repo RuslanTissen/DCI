8042d754fd3cece74e4511588eef1a5f53964e18
SFERAS <rthyssen73@gmail.com>
2023-07-06 11:44:19 -0300

    bandle done

--- Januar/19.1.22-modules-npm/print-date/src/index.js
+++ Januar/19.1.22-modules-npm/print-date/src/index.js
@@ -1,3 +1,8 @@
 import moment from "moment"
 
-console.log(moment().format("dd DD.MM"))
+console.log(moment().format("dd DD.MM"))  // npm install moment
+
+const element = document.createElement("div")
+element.innerText = "Hi from JS"
+document.body.append(element)
+
