760f904e147d49ca124183e6fcaec4a318d512e6
SFERAS <rthyssen73@gmail.com>
2023-07-10 23:49:01 -0300

    asynk in progress

--- Januar/20.1.2023-Async/index.js
+++ Januar/20.1.2023-Async/index.js
@@ -1,20 +1,91 @@
+// function addUser() {
+// 	function actallyAddUser() {
+// 		users.push("Veeera")
+// 		console.log("Added user", users)
+// 	}
+// 	setTimeout(actallyAddUser, Math.random() * 2000)
+// }
+
+// function deleteUser() {
+// 	function actuallyDeleteUser() {
+// 		users.pop()
+// 		console.log("Deleted user", users)
+// 	}
+// 	setTimeout(actuallyDeleteUser, Math.random() * 5000)
+// }
+
+// addUser()
+// deleteUser() // sometimes, delete comletes Before add!
+
+
+// var promise = new Promise((resolve, reject) => {
+
+// 	function doStuff() {
+// 		const random = Math.round(Math.random())
+// 		if (random === 1) {
+// 			resolve("Greate Success")
+// 		} else {
+// 			reject("Utter Fialure")
+// 		}
+// 	}
+
+// 	setTimeout(doStuff, 2000)
+// })
+
 const users = []
 
+// Why not callback?
+function addUser2(cb) {
+	return new Promise((resolve, reject) => {
+		function actallyAddUser() {
+			users.push("Veeera")
+			cb("Yay, added user")
+		}
+		setTimeout(actallyAddUser, Math.random() * 2000)
+	})
+}
+
+// callback hell
+// the piramid of doom
+// adduser2(() => {
+// 	deleteUser((msg) => {
+// 		updateUser((msg2)=>{
+// 			checkLogin(printGreeting)
+// 		})
+// 	})
+// })
+
 function addUser() {
-	function actallyAddUser() {
-		users.push("Veeera")
-		console.log("Added user", users)
-	}
-	setTimeout(actallyAddUser, Math.random() * 2000)
+	return new Promise((resolve, reject) => {
+		function actallyAddUser() {
+			users.push("Veeera")
+			resolve("Yay, added user")
+		}
+		setTimeout(actallyAddUser, Math.random() * 2000)
+	})
 }
 
 function deleteUser() {
-	function actuallyDeleteUser() {
-		users.pop()
-		console.log("Deleted user", users)
-	}
-	setTimeout(actuallyDeleteUser, Math.random() * 5000)
+	return new Promise((resolve, reject) => {
+
+		function actuallyDeleteUser() {
+			users.pop()
+			const coinFlip = Math.random() > 0.5
+			if (coinFlip) {
+				resolve("Deleted user successfully")
+			} else {
+				reject("Failed to deleteuser:((((")
+			}
+		}
+		setTimeout(actuallyDeleteUser, Math.random() * 5000)
+	})
 }
 
-addUser() 
-deleteUser() // sometimes, delete comletes Before add!
+// if you have many promises one after the other
+// FIRST, the not so nice syntax
+const p = addUser()
+p.then((result) => {
+	console.log("Done adding user!", result)
+	deleteUser()
+	console.log(users)
+})
