ee978c48eff9af864aaa909fda62a9bb9a174c2b
SFERAS <rthyssen73@gmail.com>
2023-07-24 12:05:02 -0300

    componenets in progress

--- Januar/31.1.22-SPA-Components-Alerts/src/App.jsx
+++ Januar/31.1.22-SPA-Components-Alerts/src/App.jsx
@@ -1,3 +1,9 @@
-export default function App(){
-	return <p>Hello form APP!</p>
+import Alert from "./components/Alert.jsx";
+
+
+export default function App() {
+	return (
+		<p>Hello form APP!</p>,
+		<Alert />
+	)
 }
